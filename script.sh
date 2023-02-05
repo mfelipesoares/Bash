ea9354d8c26328bf8a160c950191e68f883f9b22
Felipe Soares <mfelipesoaress@hotmail.com>
2023-02-05 17:05:45 -0300

    a

--- script.sh
+++ script.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+
+# Encadeamento condicional && -> se sucesso; || -> se erro.
+true && echo beleza
+false && echo nao foi executado
+false || echo foi executado
+true || echo nao foi executado
+# Execução assícrona: &
+#Pipe: |    Exemplo:
+echo  Marcos Felipe | grep 'F'
+
+
+
+
+
+
+
+exit
+
